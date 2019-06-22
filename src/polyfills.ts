aef6a78927740d96be182d77439106e5e5d75362
Yuri Sulyma <yuri@lmqm.xyz>
2019-06-22 10:32:22 -0500

    Polyfill correctly when support exists

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,22 +1,22 @@
 export function requestFullScreen(): void {
-  for (const method of ['webkitRequestFullscreen', 'mozRequestFullScreen', 'msRequestFullscreen'])
+  for (const method of ["requestFullscreen", "webkitRequestFullscreen", "mozRequestFullScreen", "msRequestFullscreen"])
     if (document.body[method])
       return document.body[method]();
 }
 
 export function exitFullScreen(): void {
-  for (const method of ['webkitExitFullscreen', 'mozCancelFullScreen', 'msExitFullscreen'])
+  for (const method of ["exitFullscreen", "webkitExitFullscreen", "mozCancelFullScreen", "msExitFullscreen"])
     if (document[method])
       return document[method]();
 }
 
 export function isFullScreen(): boolean {
-  for (const prop of ['webkitIsFullScreen', 'mozFullScreen'])
+  for (const prop of ["fullscreen", "webkitIsFullScreen", "mozFullScreen"])
     if (document[prop] !== undefined)
       return document[prop];
 }
 
 export function onFullScreenChange(callback: EventListener) {
-  for (const event of ['webkitfullscreenchange', 'mozfullscreenchange', 'MSFullscreenChange'])
+  for (const event of ["fullscreenchange", "webkitfullscreenchange", "mozfullscreenchange", "MSFullscreenChange"])
     document.addEventListener(event, callback);
 }
