14a2542c4c4d3285907749c7db027fbae6c600a3
Yuri Sulyma <yuri@lmqm.xyz>
2019-08-28 12:49:35 -0400

    Remove graphics utils

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,10 @@
 import * as animation from "./utils/animation";
 import * as authoring from "./utils/authoring";
 // import * as dom from './utils/dom';
-import * as graphics from "./utils/graphics";
 import * as interactivity from "./utils/interactivity";
 import * as media from "./utils/media";
 import * as misc from "./utils/misc";
 import * as react from "./utils/react-utils";
 import * as time from "./utils/time";
 
-export {animation, authoring, graphics, interactivity, media, misc, react, time};
+export {animation, authoring, interactivity, media, misc, react, time};
