c98ff5a9d9eb3dd89fcc655c8409bd060d06116b
Yuri Sulyma <yuri@lmqm.xyz>
2019-08-29 01:43:02 -0400

    Deprecate pos() from authoring utils

--- src/utils/authoring.tsx
+++ src/utils/authoring.tsx
@@ -1,38 +1,7 @@
-import * as React from "react";
-import aspectRatio from "../aspectRatio";
-
-interface StyleBlock {
-  style?: any;
-}
-
-// position an element
-export function pos(x?: number, y?: number, w?: number, h?: number): StyleBlock;
-export function pos(args: {x?: number; y?: number; w?: number; h?: number}): StyleBlock;
-export function pos(...args: any[]): StyleBlock {
-  let x, y, w, h;
-
-  if (args.length === 1 && typeof args[0] === "object") {
-    ({x, y, w, h} = args[0]);
-  } else {
-    [x, y, w, h] = args;
-  }
-
-  const style: any = {position: "absolute"};
-  
-  if (x !== undefined)
-    style.left = `${x * aspectRatio}vmin`;
-  if (y !== undefined)
-    style.top = `${y * aspectRatio}vmin`;
-  if (w !== undefined)
-    style.width = `${w * aspectRatio}vmin`;
-  if (h !== undefined)
-    style.height = `${h * aspectRatio}vmin`;
-
-  return {style};
-};
+import {CSSProperties} from "react";
 
 // conditional display
-export function showIf(cond: boolean): StyleBlock {
+export function showIf(cond: boolean): {style?: CSSProperties} {
   if (!cond) return {
     style: {
       opacity: 0,
@@ -48,13 +17,9 @@ export function during(prefix: string) {
   };
 }
 
-export function from(first: string, last: string) {
+export function from(first: string, last?: string) {
   return {
     ["data-from-first"]: first,
     ["data-from-last"]: last
   };
-}
-
-export function wrapIf(wrap: boolean, Tag: string, content: React.ReactChild) {
-  return wrap ? (<Tag>{content}</Tag>) : content;
 }
