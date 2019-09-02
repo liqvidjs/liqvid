1d6d8832405a69205d30a6c749663e032e601d03
Yuri Sulyma <yuri@lmqm.xyz>
2019-09-02 14:30:49 -0400

    Deprecate Block

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Audio from "./Audio";
-import Block from "./Block";
 import Cursor from "./Cursor";
 import IdMap from "./IdMap";
 import Playback from "./playback";
@@ -8,4 +7,4 @@ import Script from "./script";
 import * as Utils from "./utils";
 import Video from "./Video";
 
-export {Audio, Block, Cursor, IdMap, Playback, Player, Script, Utils, Video};
+export {Audio, Cursor, IdMap, Playback, Player, Script, Utils, Video};
