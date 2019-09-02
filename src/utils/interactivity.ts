b5e7e3728293debdc31bceb10a19362e8f519801
Yuri Sulyma <yuri@lmqm.xyz>
2019-09-02 15:08:20 -0400

    Provide hit on down event in interactivity

--- src/utils/interactivity.ts
+++ src/utils/interactivity.ts
@@ -7,31 +7,33 @@ function isReactMouseEvent<T>(
 }
 
 export function dragHelper<T extends Node>(
-  move: (e: MouseEvent | TouchEvent, o: {x: number; y: number; dx: number; dy: number}) => void,
+  move: (e: MouseEvent | TouchEvent, hit: {x: number; y: number; dx: number; dy: number}) => void,
   down: (
     e: MouseEvent | React.MouseEvent<T> | TouchEvent | React.TouchEvent<T>,
+    hit: {x: number; y: number},
     upHandler: (e: MouseEvent | TouchEvent) => void,
     moveHandler: (e: MouseEvent | TouchEvent) => void
   ) => void = () => {},
-  up: (e: MouseEvent | TouchEvent) => void = () => {}
+  up: (e: MouseEvent | TouchEvent, hit: {x: number; y: number; dx: number; dy: number}) => void = () => {}
 ) {
   return (e: MouseEvent | React.MouseEvent<T> | TouchEvent | React.TouchEvent<T>) => {
-    let upHandler: ((e: MouseEvent) => void) | ((e: TouchEvent) => void),
-        moveHandler: ((e: MouseEvent) => void) | ((e: TouchEvent) => void);
     if (e instanceof MouseEvent || isReactMouseEvent(e)) {
       if (e.button !== 0) return;
 
       let lastX = e.clientX,
           lastY = e.clientY;
 
-      upHandler = (e: MouseEvent) => {
+      const upHandler = (e: MouseEvent) => {
+        const dx = e.clientX - lastX,
+              dy = e.clientY - lastY;
+
         document.body.removeEventListener("mousemove", moveHandler);
         window.removeEventListener("mouseup", upHandler);
 
-        return up(e);
+        return up(e, {x: e.clientX, y: e.clientY, dx, dy});
       };
 
-      moveHandler = (e: MouseEvent) => {
+      const moveHandler = (e: MouseEvent) => {
         const dx = e.clientX - lastX,
               dy = e.clientY - lastY;
 
@@ -43,6 +45,8 @@ export function dragHelper<T extends Node>(
 
       document.body.addEventListener("mousemove", moveHandler, false);
       window.addEventListener("mouseup", upHandler, false);
+
+      return down(e, {x: lastX, y: lastY}, upHandler, moveHandler);
     } else {
       e.preventDefault();
       const touches = e.changedTouches;
@@ -52,43 +56,44 @@ export function dragHelper<T extends Node>(
       let lastX = touches[0].clientX,
           lastY = touches[0].clientY;
 
-      upHandler = (e: TouchEvent) => {
+      const upHandler = (e: TouchEvent) => {
         e.preventDefault();
 
-        window.removeEventListener("touchend", moveHandler);
-        window.removeEventListener("touchcancel", upHandler);
-        window.removeEventListener("touchmove", moveHandler);
-
-        return up(e);
-      };
-
-      moveHandler = (e: TouchEvent) => {
-        e.preventDefault();
         for (const touch of Array.from(e.changedTouches)) {
           if (touch.identifier !== touchId) continue;
 
           const dx = touch.clientX - lastX,
                 dy = touch.clientY - lastY;
+          
+          window.removeEventListener("touchend", moveHandler);
+          window.removeEventListener("touchcancel", upHandler);
+          window.removeEventListener("touchmove", moveHandler);
 
+          return up(e, {x: touch.clientX, y: touch.clientY, dx, dy});
+        }
+      };
+
+      const moveHandler = (e: TouchEvent) => {
+        e.preventDefault();
+        for (const touch of Array.from(e.changedTouches)) {
+          if (touch.identifier !== touchId) continue;
+          
+          const dx = touch.clientX - lastX,
+                dy = touch.clientY - lastY;
+          
           lastX = touch.clientX;
           lastY = touch.clientY;
-
+          
           return move(e, {x: touch.clientX, y: touch.clientY, dx, dy});
         }
       };
 
-      window.addEventListener("touchend", upHandler, false);
-      window.addEventListener("touchcancel", upHandler, false);
-      window.addEventListener("touchmove", moveHandler, false);
+      window.addEventListener("touchend", upHandler, {capture: false, passive: false});
+      window.addEventListener("touchcancel", upHandler, {capture: false, passive: false});
+      window.addEventListener("touchmove", moveHandler, {capture: false, passive: false});
+
+      return down(e, {x: lastX, y: lastY}, upHandler, moveHandler);
     }
-
-    // oh no oh no oh no
-    // const _stopPropagation = e.stopPropagation.bind(e);
-    // e.stopPropagation = () => {
-    //   _stopPropagation();
-    // };
-
-    return down(e, upHandler, moveHandler);
   };
 }
 
@@ -98,15 +103,11 @@ type Arg2 = DHR extends (a: infer A, b: infer B, c: infer C) => void ? B : unkno
 type Arg3 = DHR extends (a: infer A, b: infer B, c: infer C) => void ? C : unknown;
 
 // for use in React (ugh...)
-export function dragHelperReact<T>(move: Arg1, down: Arg2, up: Arg3) {
+export function dragHelperReact<T>(move: Arg1, down?: Arg2, up?: Arg3) {
   const listener = dragHelper(move, down, up);
   return {
     onMouseDown: listener,
-    onMouseUp: (e: React.MouseEvent<T>) => {
-      e.persist();
-      // this sucks
-      e[Player.ignoreCanvasClick] = true;
-    },
+    onMouseUp: Player.preventCanvasClick,
     onTouchStart: listener
   };
 }
