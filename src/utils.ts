0457c71958e011283ca34805d0fef18f38aa22c6
Yuri Sulyma <yuri@lmqm.xyz>
2020-01-08 02:02:32 -0700

    Utils.replayData

--- src/utils.ts
+++ src/utils.ts
@@ -7,6 +7,7 @@ import * as media from "./utils/media";
 import * as misc from "./utils/misc";
 import * as mobile from "./utils/mobile";
 import * as react from "./utils/react-utils";
+import * as replayData from "./utils/replay-data";
 import * as time from "./utils/time";
 
-export {animation, authoring, interactivity, media, misc, mobile, react, time};
+export {animation, authoring, interactivity, media, misc, mobile, react, replayData, time};
