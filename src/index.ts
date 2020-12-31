b1bade1385e44cfeffb0cf78bfe5744962c6e128
Yuri Sulyma <yuri@lmqm.xyz>
2020-12-31 05:58:20 -0500

    Export controls

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import Audio from "./Audio";
-import Broadcast from "./broadcast";
 import IdMap from "./IdMap";
+import KeyMap from "./keymap";
 import Playback from "./playback";
 import Player from "./Player";
 import Script from "./script";
 import * as Utils from "./utils";
 import Video from "./Video";
 
-export {Audio, Broadcast, IdMap, Playback, Player, Script, Utils, Video};
+export {Audio, IdMap, KeyMap, Playback, Player, Script, Utils, Video};
 export * from "./hooks";
+
+import FullScreen from "./controls/FullScreen";
+import PlayPause from "./controls/PlayPause";
+import Settings from "./controls/Settings";
+import TimeDisplay from "./controls/TimeDisplay";
+import Volume from "./controls/Volume";
+export const Controls = {FullScreen, PlayPause, Settings, TimeDisplay, Volume};
