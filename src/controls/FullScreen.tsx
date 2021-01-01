2b77a03b6feebe510e558656b503a585639f6322
Yuri Sulyma <yuri@lmqm.xyz>
2020-12-31 20:11:34 -0500

    Linting

--- src/controls/FullScreen.tsx
+++ src/controls/FullScreen.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {useCallback, useEffect, useMemo, useState} from "react";
+import {useCallback, useEffect, useMemo} from "react";
 
 import {requestFullScreen, exitFullScreen, isFullScreen, onFullScreenChange} from "../fake-fullscreen";
 import {usePlayer} from "../hooks";
@@ -30,19 +30,19 @@ export default function FullScreen() {
   return (
     <svg className="rp-controls-fullscreen" {...events} viewBox="0 0 36 36">
       {isFullScreen() ?
-      <>
-        <path fill="white" d="M 14 14 h -4 v 2 h 6 v -6 h -2 v 4 z"/>
-        <path fill="white" d="M 22 14 v -4 h -2 v 6 h 6 v -2 h -4 z"/>
-        <path fill="white" d="M 20 26 h 2 v -4 h 4 v -2 h -6 v 6 z"/>
-        <path fill="white" d="M 10 22 h 4 v 4 h 2 v -6 h -6 v 2 z"/>
-      </>
+        <>
+          <path fill="white" d="M 14 14 h -4 v 2 h 6 v -6 h -2 v 4 z"/>
+          <path fill="white" d="M 22 14 v -4 h -2 v 6 h 6 v -2 h -4 z"/>
+          <path fill="white" d="M 20 26 h 2 v -4 h 4 v -2 h -6 v 6 z"/>
+          <path fill="white" d="M 10 22 h 4 v 4 h 2 v -6 h -6 v 2 z"/>
+        </>
         :
-      <>
-        <path fill="white" d="M 10 16 h 2 v -4 h 4 v -2 h -6 v 6 z"/>
-        <path fill="white" d="M 20 10 v 2 h 4 v 4 h 2 v -6 h -6 z"/>
-        <path fill="white" d="M 24 24 h -4 v 2 h 6 v -6 h -2 v 4 z"/>
-        <path fill="white" d="M 12 20 h -2 v 6 h 6 v -2 h -4 v -4 z"/>
-      </>
+        <>
+          <path fill="white" d="M 10 16 h 2 v -4 h 4 v -2 h -6 v 6 z"/>
+          <path fill="white" d="M 20 10 v 2 h 4 v 4 h 2 v -6 h -6 z"/>
+          <path fill="white" d="M 24 24 h -4 v 2 h 6 v -6 h -2 v 4 z"/>
+          <path fill="white" d="M 12 20 h -2 v 6 h 6 v -2 h -4 v -4 z"/>
+        </>
       }
     </svg>
   );
