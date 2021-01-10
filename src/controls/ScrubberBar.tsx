a0492aeb3a5c0cabaa5b93e208be4acfdd55b765
Yuri Sulyma <yuri@lmqm.xyz>
2021-01-10 00:09:20 -0500

    Make KeyMap work better + don't throw error on unbound sequence

--- src/controls/ScrubberBar.tsx
+++ src/controls/ScrubberBar.tsx
@@ -46,16 +46,16 @@ export default function ScrubberBar(props: Props) {
 
     /* keyboard shortcuts */
     // seek 5
-    keymap.bind("arrowleft", () => playback.seek(playback.currentTime - 5000));
-    keymap.bind("arrowright", () => playback.seek(playback.currentTime + 5000));
+    keymap.bind("ArrowLeft", () => playback.seek(playback.currentTime - 5000));
+    keymap.bind("ArrowRight", () => playback.seek(playback.currentTime + 5000));
 
     // seek 10
-    keymap.bind("j", () => playback.seek(playback.currentTime - 10000));
-    keymap.bind("l", () => playback.seek(playback.currentTime + 10000));
+    keymap.bind("J", () => playback.seek(playback.currentTime - 10000));
+    keymap.bind("L", () => playback.seek(playback.currentTime + 10000));
 
     // seek by marker
-    keymap.bind("w", () => script.back());
-    keymap.bind("e", () => script.forward());
+    keymap.bind("W", () => script.back());
+    keymap.bind("E", () => script.forward());
 
     // percentage seeking
     keymap.bind("0,1,2,3,4,5,6,7,8,9", e => {
--- src/keymap.ts
+++ src/keymap.ts
@@ -13,6 +13,184 @@ const modifierMap = {
   Shift: "Shift"
 };
 
+const mixedCaseVals = [
+  "AltGraph",
+  "CapsLock",
+  "FnLock",
+  "NumLock",
+  "ScrollLock",
+  "SymbolLock",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowUp",
+  "PageDown",
+  "PageUp",
+  "CrSel",
+  "EraseEof",
+  "ExSel",
+  "ContextMenu",
+  "ZoomIn",
+  "ZoomOut",
+  "BrightnessDown",
+  "BrightnessUp",
+  "LogOff",
+  "PowerOff",
+  "PrintScreen",
+  "WakeUp",
+  "AllCandidates",
+  "CodeInput",
+  "FinalMode",
+  "GroupFirst",
+  "GroupLast",
+  "GroupNext",
+  "GroupPrevious",
+  "ModeChange",
+  "NextCandidate",
+  "NonConvert",
+  "PreviousCandidate",
+  "SingleCandidate",
+  "HangulMode",
+  "HanjaMode",
+  "JunjaMode",
+  "HiraganaKatakana",
+  "KanaMode",
+  "KanjiMode",
+  "ZenkakuHanaku",
+  "AppSwitch",
+  "CameraFocus",
+  "EndCall",
+  "GoBack",
+  "GoHome",
+  "HeadsetHook",
+  "LastNumberRedial",
+  "MannerMode",
+  "VoiceDial",
+  "ChannelDown",
+  "ChannelUp",
+  "MediaFastForward",
+  "MediaPause",
+  "MediaPlay",
+  "MediaPlayPause",
+  "MediaRecord",
+  "MediaRewind",
+  "MediaStop",
+  "MediaTrackNext",
+  "MediaTrackPrevious",
+  "AudioBalanceLeft",
+  "AudioBalanceRight",
+  "AudioBassDown",
+  "AudioBassBoostDown",
+  "AudioBassBoostToggle",
+  "AudioBassBoostUp",
+  "AudioBassUp",
+  "AudioFaderFront",
+  "AudioFaderRear",
+  "AudioSurroundModeNext",
+  "AudioTrebleDown",
+  "AudioTrebleUp",
+  "AudioVolumeDown",
+  "AudioVolumeMute",
+  "AudioVolumeUp",
+  "MicrophoneToggle",
+  "MicrophoneVolumeDown",
+  "MicrophoneVolumeMute",
+  "MicrophoneVolumeUp",
+  "TV",
+  "TVAntennaCable",
+  "TVAudioDescription",
+  "TVAudioDescriptionMixDown",
+  "TVAudioDescriptionMixUp",
+  "TVContentsMenu",
+  "TVDataService",
+  "TVInput",
+  "TVMediaContext",
+  "TVNetwork",
+  "TVNumberEntry",
+  "TVPower",
+  "TVRadioService",
+  "TVSatellite",
+  "TVSatelliteBS",
+  "TVSatelliteCS",
+  "TVSatelliteToggle",
+  "TVTerrestrialAnalog",
+  "TVTerrestrialDigital",
+  "TVTimer",
+  "AVRInput",
+  "AVRPower",
+  "ClosedCaptionToggle",
+  "DisplaySwap",
+  "DVR",
+  "GuideNextDay",
+  "GuidePreviousDay",
+  "InstantReplay",
+  "ListProgram",
+  "LiveContent",
+  "MediaApps",
+  "MediaAudioTrack",
+  "MediaLast",
+  "MediaSkipBackward",
+  "MediaSkipForward",
+  "MediaStepBackward",
+  "MediaStepForward",
+  "MediaTopMenu",
+  "NavigateIn",
+  "NavigateNext",
+  "NavigateOut",
+  "NavigatePrevious",
+  "NextFavoriteChannel",
+  "NextUserProfile",
+  "OnDemand",
+  "PinPDown",
+  "PinPMove",
+  "PinPToggle",
+  "PinPUp",
+  "PlaySpeedDown",
+  "PlaySpeedReset",
+  "PlaySpeedUp",
+  "RandomToggle",
+  "RcLowBattery",
+  "RecordSpeedNext",
+  "RfBypass",
+  "ScanChannelsToggle",
+  "ScreenModeNext",
+  "SplitScreenToggle",
+  "STBInput",
+  "STBPower",
+  "VideoModeNext",
+  "ZoomToggle",
+  "SpeechCorrectionList",
+  "SpeechInputToggle",
+  "SpellCheck",
+  "MailForward",
+  "MailReply",
+  "MailSend",
+  "LaunchCalculator",
+  "LaunchCalendar",
+  "LaunchContacts",
+  "LaunchMail",
+  "LaunchMediaPlayer",
+  "LaunchMusicPlayer",
+  "LaunchMyComputer",
+  "LaunchPhone",
+  "LaunchScreenSaver",
+  "LaunchSpreadsheet",
+  "LaunchWebBrowser",
+  "LaunchWebCam",
+  "LaunchWordProcessor",
+  "BrowserBack",
+  "BrowserFavorites",
+  "BrowserForward",
+  "BrowserHome",
+  "BrowserRefresh",
+  "BrowserSearch",
+  "BrowserStop"
+];
+const mixedCase = {};
+for (const key of mixedCaseVals) {
+  mixedCase[key.toLowerCase()] = key;
+}
+
 const modifierOrder = ["Control", "Alt", "Shift", "Meta"];
 
 const useCode = [
@@ -46,7 +224,18 @@ export default class KeyMap {
   }
 
   static normalize(seq: string) {
-    return seq.split("+").map(titlecase).sort((a, b) => {
+    return seq.split("+").map(str => {
+      const lower = str.toLowerCase();
+
+      if (str === "")
+        return "";
+
+      if (mixedCase[lower]) {
+        return mixedCase[lower];
+      }
+
+      return str[0].toUpperCase() + lower.slice(1);
+    }).sort((a, b) => {
       if (a in modifierMap) {
         if (b in modifierMap) {
           return modifierOrder.indexOf(a) - modifierOrder.indexOf(b);
@@ -105,7 +294,7 @@ export default class KeyMap {
 
   getHandlers(seq: string) {
     if (!this.bindings.hasOwnProperty(seq))
-      throw new Error();
+      return [];
     return this.bindings[seq].slice();
   }
 
