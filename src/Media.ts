1ce43ea0a5a728b8274196c9b6e9722ae95c61c0
Yuri Sulyma <>
2021-06-07 19:38:32 -0400

    Work correctly with keyboard play/pause buttons

--- src/Media.ts
+++ src/Media.ts
@@ -3,6 +3,7 @@ import * as React from "react";
 import {awaitMediaCanPlay, awaitMediaCanPlayThrough} from "./utils/media";
 import {between, bind} from "./utils/misc";
 
+import type Playback from "./playback";
 import Player from "./Player";
 
 interface Props extends React.HTMLAttributes<HTMLMediaElement> {
@@ -12,6 +13,7 @@ interface Props extends React.HTMLAttributes<HTMLMediaElement> {
 }
 
 export default class Media extends React.PureComponent<Props> {
+  protected playback: Playback;
   protected player: Player;
   protected domElement: HTMLMediaElement;
   start: number;
@@ -27,24 +29,26 @@ export default class Media extends React.PureComponent<Props> {
   constructor(props: Props, context: Player) {
     super(props, context);
     this.player = context;
+    this.playback = context.playback;
 
     // get the time right
     this.start = this.player.script.parseStart(this.props.start);
 
-    bind(this, ["onPause", "onPlay", "onRateChange", "onSeek", "onSeeking", "onTimeUpdate", "onVolumeChange"]);
+    bind(this, ["pause", "play", "onPlay", "onRateChange", "onSeek", "onTimeUpdate", "onVolumeChange", "onDomPlay", "onDomPause"]);
   }
 
   componentDidMount() {
     // attach event listeners
-    const {playback} = this.player;
-    
-    playback.hub.on("pause", this.onPause);
-    playback.hub.on("play", this.onPlay);
-    playback.hub.on("ratechange", this.onRateChange);
-    playback.hub.on("seek", this.onSeek);
-    playback.hub.on("seeking", this.onSeeking);
-    playback.hub.on("timeupdate", this.onTimeUpdate);
-    playback.hub.on("volumechange", this.onVolumeChange);
+    this.playback.hub.on("pause", this.pause);
+    this.playback.hub.on("play", this.onPlay);
+    this.playback.hub.on("ratechange", this.onRateChange);
+    this.playback.hub.on("seek", this.onSeek);
+    this.playback.hub.on("seeking", this.pause);
+    this.playback.hub.on("timeupdate", this.onTimeUpdate);
+    this.playback.hub.on("volumechange", this.onVolumeChange);
+
+    this.domElement.addEventListener("play", this.onDomPlay);
+    this.domElement.addEventListener("pause", this.onDomPause);
 
     // canplay/canplaythrough events
     if (this.props.obstructCanPlay) {
@@ -82,15 +86,16 @@ export default class Media extends React.PureComponent<Props> {
   }
 
   componentWillUnmount() {
-    const {playback} = this.player;
+    this.playback.hub.off("pause", this.pause);
+    this.playback.hub.off("play", this.onPlay);
+    this.playback.hub.off("ratechange", this.onRateChange);
+    this.playback.hub.off("seek", this.onSeek);
+    this.playback.hub.off("seeking", this.pause);
+    this.playback.hub.off("timeupdate", this.onTimeUpdate);
+    this.playback.hub.off("volumechange", this.onVolumeChange);
 
-    playback.hub.off("pause", this.onPause);
-    playback.hub.off("play", this.onPlay);
-    playback.hub.off("ratechange", this.onRateChange);
-    playback.hub.off("seek", this.onSeek);
-    playback.hub.off("seeking", this.onSeeking);
-    playback.hub.off("timeupdate", this.onTimeUpdate);
-    playback.hub.off("volumechange", this.onVolumeChange);
+    this.domElement.removeEventListener("pause", this.onDomPause);
+    this.domElement.removeEventListener("play", this.onDomPlay);
 
     this.player.unregisterBuffer(this.domElement);
   }
@@ -100,33 +105,36 @@ export default class Media extends React.PureComponent<Props> {
     return this.start + this.domElement.duration * 1000;
   }
 
-  onPlay() {
-    this.onTimeUpdate(this.player.playback.currentTime);
+  pause() {
+    this.domElement.removeEventListener("pause", this.onDomPause);
+    this.domElement.pause();
+    this.domElement.addEventListener("pause", this.onDomPause);
   }
 
-  onPause() {
-    this.domElement.pause();
+  play() {
+    this.domElement.removeEventListener("play", this.onDomPlay);
+    const promise = this.domElement.play();
+    this.domElement.addEventListener("play", this.onDomPlay);
+    return promise;
+  }
+
+  onPlay() {
+    this.onTimeUpdate(this.playback.currentTime);
   }
 
   onRateChange() {
-    this.domElement.playbackRate = this.player.playback.playbackRate;
-  }
-
-  onSeeking() {
-    this.domElement.pause();
+    this.domElement.playbackRate = this.playback.playbackRate;
   }
 
   onSeek(t: number) {
-    const {playback} = this.player;
-
     if (between(this.start, t, this.end)) {
       this.domElement.currentTime = (t - this.start) / 1000;
 
-      if (this.domElement.paused && !playback.paused && !playback.seeking)
-        this.domElement.play().catch(this.player.playback.pause);
+      if (this.domElement.paused && !this.playback.paused && !this.playback.seeking)
+        this.play().catch(this.playback.pause);
     } else {
       if (!this.domElement.paused)
-        this.domElement.pause();
+        this.pause();
     }
   }
 
@@ -135,17 +143,31 @@ export default class Media extends React.PureComponent<Props> {
       if (!this.domElement.paused) return;
 
       this.domElement.currentTime = (t - this.start) / 1000;
-      this.domElement.play().catch(this.player.playback.pause);
+      this.play().catch(this.playback.pause);
     } else {
       if (!this.domElement.paused)
-        this.domElement.pause();
+        this.pause();
     }
   }
 
   onVolumeChange() {
-    const {playback} = this.player;
+    this.domElement.volume = this.playback.volume;
+    this.domElement.muted = this.playback.muted;
+  }
 
-    this.domElement.volume = playback.volume;
-    this.domElement.muted = playback.muted;
+   onDomPlay() {
+    if (this.playback.paused) {
+      this.playback.hub.off("play", this.onPlay);
+      this.playback.play();
+      this.playback.hub.on("play", this.onPlay);
+    }
+  }
+
+  onDomPause() {
+    if (!this.playback.seeking && !this.playback.paused) {
+      this.playback.hub.off("pause", this.pause);
+      this.playback.pause();
+      this.playback.hub.on("pause", this.pause);
+    }
   }
 }
