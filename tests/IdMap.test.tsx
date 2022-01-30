3719001130d8ba5ff43f9554a678687de2d5d473
Yuri Sulyma <yuri@liqvidjs.org>
2022-01-30 05:18:37 -0500

    Add tests for IdMap

---
+++ tests/IdMap.test.tsx
@@ -0,0 +1,42 @@
+import * as React from "react";
+import {render} from "@testing-library/react";
+
+import "./matchMedia.mock";
+import "./DocumentTimeline.mock";
+
+import {IdMap} from "../dist/liqvid";
+
+function Component() {
+  return (
+    <div>
+      <IdMap>
+        <h1 id="a">Hello World</h1>
+      </IdMap>
+    </div>
+  );
+}
+
+describe("IdMap", () => {
+  const objects = {
+    "a": {
+      className: "demo-a",
+      style: {
+        color: "red"
+      }
+    }
+  };
+
+  test("root works", () => {
+    render(<IdMap map={objects}><h1 id="a">Hello World</h1></IdMap>);
+    const h1 = document.querySelector("h1");
+    expect(h1.className).toBe("demo-a");
+    expect(h1.style.color).toBe("red");
+  });
+
+  test("nested works", () => {
+    render(<IdMap map={objects}><Component/></IdMap>);
+    const h1 = document.querySelector("h1");
+    expect(h1.className).toBe("demo-a");
+    expect(h1.style.color).toBe("red");
+  });
+});
