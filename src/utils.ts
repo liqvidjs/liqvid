913684d1856dda0d8a95bc25e7b3be83edaa1645
Yuri Sulyma <yuri@liqvidjs.org>
2022-03-10 00:57:52 -0500

    Move react-utils to @liqvid/utils/react

--- src/utils.ts
+++ src/utils.ts
@@ -7,7 +7,7 @@ export * as json from "@liqvid/utils/json";
 export * as media from "./utils/media";
 export * as misc from "@liqvid/utils/misc";
 export * as mobile from "./utils/mobile";
-export * as react from "./utils/react-utils";
+export * as react from "@liqvid/utils/react";
 export * as replayData from "@liqvid/utils/replay-data";
 export * as svg from "@liqvid/utils/svg";
 export * as time from "@liqvid/utils/time";
