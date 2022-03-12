21a50aa28b6df774f85c688ed8312816f1df5033
Yuri Sulyma <yuri@liqvidjs.org>
2022-03-12 06:31:00 -0500

    Use Symbol for context in @utils

--- packages/keymap/src/react.ts
+++ packages/keymap/src/react.ts
@@ -1,10 +1,20 @@
 import {createContext, useContext} from "react";
 import {Keymap} from ".";
 
+const symbol = Symbol.for("@lqv/keymap");
+
+type GlobalThis = {
+  [symbol]: React.Context<Keymap>;
+}
+
+if (!(symbol in globalThis)) {
+  (globalThis as unknown as GlobalThis)[symbol] = createContext<Keymap>(null);
+}
+
 /**
  * {@link React.Context} used to access ambient Keymap
  */
-export const KeymapContext = createContext<Keymap>(null);
+export const KeymapContext = (globalThis as unknown as GlobalThis)[symbol];
 
 /** Access the ambient {@link Keymap} */
 export function useKeymap() {
