2f653ce38b5a79f44143e37e23fb43fa062e71f7
Yuri Sulyma <yuri@liqvidjs.org>
2022-03-27 17:23:05 -0400

    Fix xyjax animations

--- packages/xyjax/src/index.ts
+++ packages/xyjax/src/index.ts
@@ -2,32 +2,16 @@
   XyJax shenanigans
 */
 
-import {animate} from "@liqvid/utils/animation";
 import {lerp} from "@liqvid/utils/misc";
-import {usePlayer, useTime} from "liqvid";
+import {usePlayback, useTime} from "liqvid";
 import {useCallback, useEffect, useRef} from "react";
 import {Handle} from "@liqvid/mathjax";
-
-type AnimateOptions = Parameters<typeof animate>[0] extends (infer K) | (infer K)[] ? K : never;
-
-type Opts = Omit<AnimateOptions, "startTime"> & {
-  startTime: number | string;
-}
 
 interface Coords {
   x1?: number;
   y1?: number;
   x2?: number;
   y2?: number;
-}
-
-/**
- * Animate opacity of nodes
- */
-export function a$opacity(u: number, nodes: SVGElement[]): void {
-  for (const node of nodes) {
-    node.style.opacity = u.toString();
-  }
 }
 
 /**
@@ -46,22 +30,20 @@ export function useAnimateArrows(o: {
   /** Reference to container {@link MJX} */
   ref: React.MutableRefObject<Handle>;
 
-  /** Animation function for arrow head */
-  headFn: (t: number) => number;
-
   /** Animation function for arrow tail */
   tailFn: (t: number) => number;
 
-  /** Animation function for arrow label */
-  labelFn: (t: number) => number;
+  /** Fade options for arrow head */
+  headFade: KeyframeEffectOptions;
+
+  /** Fade options for arrow label */
+  labelFade: KeyframeEffectOptions;
 }, deps?: React.DependencyList): void {
-  const {playback} = usePlayer();
+  const playback = usePlayback();
   const tail = useRef<SVGLineElement>();
-  // const head = useRef<SVGPathElement[]>([]);
-  // const label = useRef<SVGElement[]>([]);
   const init = useRef<Coords>({});
 
-  /* fading functions */
+  /* fading function */
   const fadeTail = useCallback((u: number) => {
     if (!tail.current)
       return;
@@ -77,109 +59,34 @@ export function useAnimateArrows(o: {
     }
   }, []);
 
-  /* fix nodes on rerender */
+  /* initialize */
   useEffect(() => {
-    const getNodes = () => {
-      if (!o.ref.current) {
-        return;
-      }
-
-      const reAnimate = init.current.hasOwnProperty("y2");
-
-      // tail
+    o.ref.current.ready.then(() => {
+      /* tail animation */
       tail.current = o.ref.current.domElement.querySelector(o.tail);
 
-      const x1 = parseFloat(tail.current.getAttribute("x1"));
-      const y1 = parseFloat(tail.current.getAttribute("y1"));
-
-      if (reAnimate) {
-        init.current.x2 *= parseFloat(tail.current.getAttribute("x1")) / init.current.x1;
-        init.current.y2 *= parseFloat(tail.current.getAttribute("y1")) / init.current.y1;
-      } else {
-        for (const key of ["x2", "y2"]) {
-          init.current[key] = parseFloat(tail.current.getAttribute(key));
-        }
+      for (const key of ["x1", "y1", "x2", "y2"] as (keyof Coords)[]) {
+        init.current[key] = parseFloat(tail.current.getAttribute(key));
       }
-      init.current.x1 = x1;
-      init.current.y1 = y1;
 
-      // re-animate
-      if (reAnimate) {
-        fadeTail(o.tailFn(playback.currentTime));
+      fadeTail(o.tailFn(playback.currentTime));
+
+      /* head animation */
+      const headNodes = Array.from(o.ref.current.domElement.querySelectorAll(o.head));
+      for (const head of headNodes) {
+        playback.newAnimation([{opacity: 0}, {opacity: 1}], o.headFade)(head);
       }
-    };
 
-    o.ref.current.ready.then(getNodes);
-    // o.ref.current.hub.on("Rerender", getNodes);
-
-    return () => {
-      // o.ref.current.hub.off("Rerender", getNodes);
-    };
+      /* label animation */
+      const labelNodes = Array.from(o.ref.current.domElement.querySelectorAll(o.label));
+      for (const label of labelNodes) {
+        playback.newAnimation([{opacity: 0}, {opacity: 1}], o.labelFade)(label);
+      }
+    });
   }, deps);
 
+  // tail animation
   useTime(fadeTail, o.tailFn, deps);
-
-  /* uMA api */
-  useMathAnimation({
-    ref: o.ref,
-    selector: o.head,
-    fn: o.headFn,
-    cb: a$opacity
-  }, deps);
-
-  useMathAnimation({
-    ref: o.ref,
-    selector: o.label,
-    fn: o.labelFn,
-    cb: a$opacity
-  }, deps);
-}
-
-/**
- * Animate inside MathJax
- */
-export function useMathAnimation(o: {
-  /** Ref to {@link MJX} container */
-  ref: React.MutableRefObject<Handle>;
-
-  /** CSS selector for animation target */
-  selector: string;
-
-  /** Animation easing */
-  fn: (t: number) => number;
-
-  /** Animation callback */
-  cb: (u: number, nodes: SVGElement[]) => void;
-}, deps?: React.DependencyList): void {
-  const {playback} = usePlayer();
-  const nodes = useRef<SVGElement[]>([]);
-
-  useEffect(() => {
-    let rerender = false;
-
-    const getNodes = () => {
-      if (!o.ref.current) {
-        return;
-      }
-
-      nodes.current = Array.from(o.ref.current.domElement.querySelectorAll(o.selector));
-
-      if (rerender) {
-        o.cb(o.fn(playback.currentTime), nodes.current);
-      } else {
-        rerender = true;
-      }
-    };
-
-    o.ref.current.ready.then(getNodes);
-    // o.ref.current.hub.on("Rerender", getNodes);
-
-    return () => {
-      // o.ref.current.hub.off("Rerender", getNodes);
-    };
-  }, deps);
-
-  useTime(u => o.cb(u, nodes.current), o.fn, deps);
 }
 
 // absolutely bonkers interception
@@ -263,8 +170,41 @@ export function extendXY(): void {
 }
 
 const MAP = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const to_b58 = function (B: Uint8Array, A: string) {let d = [], s = "", i, j, c, n; for (i in B) {j = 0, c = B[i]; s += c || s.length ^ i ? "" : 1; while (j in d || c) {n = d[j]; n = n ? n * 256 + c : c; c = n / A.length | 0; d[j] = n % A.length; j++;} } while (j--) s += A[d[j]]; return s;};
-const from_b58 = function (S: string, A: string) {let d = [], b = [], i, j, c, n; for (i in S) {j = 0, c = A.indexOf(S[i]); if (c < 0) return undefined; c || b.length ^ i ? i : b.push(0); while (j in d || c) {n = d[j]; n = n ? n * A.length + c : c; c = n >> 8; d[j] = n % 256; j++;} } while (j--) b.push(d[j]); return new Uint8Array(b);};
+function to_b58(B: Uint8Array, A: string) {
+  let d: number[] = [],
+    s = "",
+    j: number, c: number, n: number;
+  for (let i = 0; i < B.length; ++i) {
+    j = 0, c = B[i];
+    s += c || s.length ^ i ? "" : 1;
+    while (j in d || c) {
+      n = d[j];
+      n = n ? n * 256 + c : c; c = n / A.length | 0;
+      d[j] = n % A.length; j++;
+    }
+  }
+  while (j--) s += A[d[j]];
+  return s;
+}
+
+function from_b58(S: string, A: string) {
+  let d: number[] = [],
+    b = [],
+    j: number, c: number, n: number;
+  for (let i = 0; i < S.length; ++i) {
+    j = 0, c = A.indexOf(S[i]);
+    if (c < 0) return undefined;
+    c || b.length ^ i ? i : b.push(0);
+    while (j in d || c) {
+      n = d[j];
+      n = n ? n * A.length + c : c;
+      c = n >> 8;
+      d[j] = n % 256; j++;
+    }
+  }
+  while (j--) b.push(d[j]);
+  return new Uint8Array(b);
+}
 
 /**
  * Encode a hex color for XyJax
