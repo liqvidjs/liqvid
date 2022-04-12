b525df9f003f234ccd2b2d853b1528a4aff318b2
Yuri Sulyma <yuri@liqvidjs.org>
2022-04-12 17:53:34 -0400

    Add onClick to @liqvid/utils + fix its types

---
+++ packages/utils/src/interaction.ts
@@ -0,0 +1,197 @@
+/* https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/removeEventListener#matching_event_listeners_for_removal */
+declare global {
+  interface EventListenerOptions {
+    passive?: boolean;
+  }
+}
+
+/**
+  Whether any available input mechanism can hover over elements. This is used as a standin for desktop/mobile.
+*/
+export const anyHover = window?.matchMedia?.("(any-hover: hover)")?.matches;
+
+/**
+ * Helper for implementing drag functionality, abstracting over mouse vs touch events.
+ * @returns An event listener which should be added to both `mousedown` and `touchstart` events.
+ */
+export function dragHelper(
+  /** Callback for dragging (pointer is moved while down). */
+  move: (
+    /** The underlying `mousemove` or `touchmove` event */
+    e: MouseEvent | TouchEvent,
+    /** Information about the pointer location */
+    hit: {
+      /** Horizontal coordinate of pointer */
+      x: number;
+      /** Vertical coordinate of pointer */
+      y: number;
+      /** Horizontal displacement since last call */
+      dx: number;
+      /** Vertical displacement since last call */
+      dy: number
+    }) => void,
+  /** Callback for when dragging begins (pointer is touched). */
+  down: (
+    /** The underlying `mousedown` or `touchstart` event */
+    e: MouseEvent | TouchEvent,
+    /** Information about the pointer location */
+    hit: {
+      /** Horizontal coordinate of pointer */
+      x: number;
+      /** Vertical coordinate of pointer */
+      y: number;
+    },
+    /** The upHandler used internally by this method */
+    upHandler: (e: MouseEvent | TouchEvent) => void,
+    /** The moveHandler used internally by this method */
+    moveHandler: (e: MouseEvent | TouchEvent) => void
+  ) => void = () => {},
+  /** Callback for when dragging ends (pointer is lifted). */
+  up: (
+    /** The underlying `mouseup` or `touchcancel`/`touchend` event */
+    e: MouseEvent | TouchEvent,
+    /** Information about the pointer location */
+    hit: {
+      /** Horizontal coordinate of pointer */
+      x: number;
+      /** Vertical coordinate of pointer */
+      y: number;
+      /** Horizontal displacement since last call */
+      dx: number;
+      /** Vertical displacement since last call */
+      dy: number;
+    }) => void = () => {}
+) {
+  return (e: MouseEvent | TouchEvent) => {
+    /* click events */
+    if (e instanceof MouseEvent) {
+      if (e.button !== 0) return;
+
+      let lastX = e.clientX,
+          lastY = e.clientY;
+
+      // up
+      const upHandler = (e: MouseEvent) => {
+        const dx = e.clientX - lastX,
+              dy = e.clientY - lastY;
+
+        document.body.removeEventListener("mousemove", moveHandler);
+        window.removeEventListener("mouseup", upHandler);
+
+        return up(e, {x: e.clientX, y: e.clientY, dx, dy});
+      };
+
+      // move
+      const moveHandler = (e: MouseEvent) => {
+        const dx = e.clientX - lastX,
+              dy = e.clientY - lastY;
+
+        lastX = e.clientX;
+        lastY = e.clientY;
+
+        return move(e, {x: e.clientX, y: e.clientY, dx, dy});
+      };
+
+      document.body.addEventListener("mousemove", moveHandler, {passive: false});
+      window.addEventListener("mouseup", upHandler, {passive: false});
+
+      return down(e, {x: lastX, y: lastY}, upHandler, moveHandler);
+    } else {
+      /* touch events */
+      e.preventDefault();
+      const touches = e.changedTouches;
+
+      const touchId = touches[0].identifier;
+
+      let lastX = touches[0].clientX,
+          lastY = touches[0].clientY;
+
+      // up
+      const upHandler = (e: TouchEvent) => {
+        e.preventDefault();
+
+        for (const touch of Array.from(e.changedTouches)) {
+          if (touch.identifier !== touchId) continue;
+
+          const dx = touch.clientX - lastX,
+                dy = touch.clientY - lastY;
+
+          window.removeEventListener("touchend", upHandler, {capture: false, passive: false});
+          window.removeEventListener("touchcancel", upHandler, {capture: false, passive: false});
+          window.removeEventListener("touchmove", moveHandler, {capture: false, passive: false});
+
+          return up(e, {x: touch.clientX, y: touch.clientY, dx, dy});
+        }
+      };
+
+      // move
+      const moveHandler = (e: TouchEvent) => {
+        e.preventDefault();
+        for (const touch of Array.from(e.changedTouches)) {
+          if (touch.identifier !== touchId) continue;
+
+          const dx = touch.clientX - lastX,
+                dy = touch.clientY - lastY;
+
+          lastX = touch.clientX;
+          lastY = touch.clientY;
+
+          return move(e, {x: touch.clientX, y: touch.clientY, dx, dy});
+        }
+      };
+
+      window.addEventListener("touchend", upHandler, {capture: false, passive: false});
+      window.addEventListener("touchcancel", upHandler, {capture: false, passive: false});
+      window.addEventListener("touchmove", moveHandler, {capture: false, passive: false});
+
+      return down(e, {x: lastX, y: lastY}, upHandler, moveHandler);
+    }
+  };
+}
+
+/**
+ * Replacement for addEventListener("click") which works better on mobile.
+ * @param node Event target.
+ * @param callback Event listener.
+ * @returns A function to remove the event listener.
+*/
+export function attachClickHandler<T extends HTMLElement | SVGElement>(
+  node: T, callback: (e: (MouseEvent | TouchEvent) & {currentTarget: T}) => void
+): () => void {
+  if (anyHover) {
+    node.addEventListener("click", callback);
+    return () => {
+      node.removeEventListener("click", callback);
+    };
+  }
+
+  let touchId: number;
+
+  // touchstart handler
+  const touchStart = (e: TouchEvent): void => {
+    if (typeof touchId === "number") return;
+    touchId = e.changedTouches[0].identifier;
+  };
+
+  // touchend handler
+  const touchEnd = (e: TouchEvent): void => {
+    if (typeof touchId !== "number") return;
+    for (const touch of Array.from(e.changedTouches)) {
+      if (touch.identifier !== touchId) continue;
+
+      if (node.contains(document.elementFromPoint(touch.clientX, touch.clientY))) {
+        callback(e as TouchEvent & {currentTarget: T});
+      }
+
+      touchId = undefined;
+    }
+  };
+
+  node.addEventListener("touchstart", touchStart);
+  node.addEventListener("touchend", touchEnd);
+
+  return () => {
+    node.removeEventListener("touchstart", touchStart);
+    node.removeEventListener("touchend", touchEnd);
+  };
+}
--- packages/utils/src/react.ts
+++ packages/utils/src/react.ts
@@ -1,30 +1,5 @@
 import {Children, cloneElement, isValidElement, useMemo, useReducer, useRef} from "react";
-
-/**
- * Combine multiple refs into one
- * @param args Refs to combine
- * @returns A ref which applies all the passed refs
- */
-export function combineRefs<T>(...args: React.Ref<T>[]) {
-  return (o: T) => {
-    for (const ref of args) {
-      if (typeof ref === "function") {
-        ref(o);
-      } else if (ref === null) {
-      } else if (typeof ref === "object" && ref.hasOwnProperty("current")) {
-        (ref as React.MutableRefObject<T>).current = o;
-      }
-    }
-  };
-}
-
-/**
- * Get a function to force the component to update
- * @returns A forceUpdate() function
- */
-export function useForceUpdate(): () => void {
-  return useReducer((c: number) => c+1, 0)[1];
-}
+import {anyHover} from "./interaction";
 
 /**
   Helper for the https://github.com/facebook/react/issues/2043 workaround. Use to intercept refs and
@@ -43,6 +18,68 @@ export const captureRef = <T>(callback: (ref: T) => void, innerRef?: React.Ref<T
     (innerRef as React.MutableRefObject<T>).current = ref;
   }
 };
+
+/**
+ * Combine multiple refs into one
+ * @param args Refs to combine
+ * @returns A ref which applies all the passed refs
+ */
+export function combineRefs<T>(...args: React.Ref<T>[]): (o: T) => void {
+  return (o: T) => {
+    for (const ref of args) {
+      if (typeof ref === "function") {
+        ref(o);
+      } else if (ref === null) {
+      } else if (typeof ref === "object" && ref.hasOwnProperty("current")) {
+        (ref as React.MutableRefObject<T>).current = o;
+      }
+    }
+  };
+}
+
+/**
+ * Drop-in replacement for onClick handlers which works better on mobile.
+ * @param callback Event listener.
+ * @returns Props to attach to event target.
+*/
+export function onClick<T extends HTMLElement | SVGElement>(
+  callback: (e: React.MouseEvent<T> | (TouchEvent & {currentTarget: T})) => void
+): {onClick: typeof callback} | {onTouchStart: (e: TouchEvent) => void; onTouchEnd: (e: TouchEvent) => void} {
+  if (anyHover) {
+    return {onClick: callback};
+  } else {
+    let touchId: number,
+        target: T & EventTarget;
+
+    // touchstart handler
+    const onTouchStart = (e: TouchEvent): void => {
+      if (typeof touchId === "number")
+        return;
+      target = e.currentTarget as T;
+      touchId = e.changedTouches[0].identifier;
+    };
+
+    // touchend handler
+    const onTouchEnd = (e: TouchEvent): void => {
+      if (typeof touchId !== "number")
+        return;
+
+      for (const touch of Array.from(e.changedTouches)) {
+        if (touch.identifier !== touchId)
+          continue;
+
+        if (target.contains(document.elementFromPoint(touch.clientX, touch.clientY))) {
+          callback(e as TouchEvent & {currentTarget: T});
+        }
+
+        touchId = undefined;
+        break;
+      }
+    };
+
+    return {onTouchStart, onTouchEnd};
+  }
+}
 
 /**
  * Recursive version of {@link React.Children.map}
@@ -65,9 +102,17 @@ export function recursiveMap(
       });
     }
 
-    // @ts-ignore
+    // @ts-expect-error Not sure how to type this correctly.
     return fn(child);
   });
+}
+
+/**
+ * Get a function to force the component to update
+ * @returns A forceUpdate() function
+ */
+export function useForceUpdate(): () => void {
+  return useReducer((c: number) => c+1, 0)[1];
 }
 
 /**
