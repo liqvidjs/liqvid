6de96b65d9883a7d6ebf7fd93ee7e5ad015ed0ce
Yuri Sulyma <yuri.sulyma@protonmail.com>
2026-03-19 21:03:52 +0100

    route files

---
+++ packages/create-liqvid/templates/app-tw/ts/app/[[...route]]/page.dev-only.tsx
@@ -0,0 +1,2 @@
+export * from "@liqvid/studio/next/page";
+export { default } from "@liqvid/studio/next/page";
---
+++ packages/create-liqvid/templates/app/ts/app/api/liqvid/[[...route]]/route.dev-only.ts
@@ -0,0 +1,18 @@
+/**
+ * @file This file sets up the Liqvid development server for Next.js
+ */
+import {
+  deleteHandler,
+  getHandler,
+  patchHandler,
+  postHandler,
+  putHandler,
+} from "@liqvid/studio/next/api";
+
+import dynamicImports from "@/.dynamic-imports";
+
+export const DELETE = deleteHandler(dynamicImports);
+export const GET = getHandler(dynamicImports);
+export const PATCH = patchHandler(dynamicImports);
+export const POST = postHandler(dynamicImports);
+export const PUT = putHandler(dynamicImports);
