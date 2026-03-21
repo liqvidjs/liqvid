921d1842a8ee98489abf8d1993694afde42388fe
Yuri Sulyma <yuri.sulyma@protonmail.com>
2026-03-21 01:29:04 +0100

    script shortcuts

--- packages/script/src/react/useScript.tsx
+++ packages/script/src/react/useScript.tsx
@@ -45,6 +45,7 @@ export function ScriptProvider<M extends string>({
   shortcuts?: ScriptShortcuts;
 }) {
   const inheritedValue = useScriptOptional();
+  const keymap = useKeymapOptional();
 
   const context = propsScript ?? inheritedValue;
 
